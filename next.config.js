a220117f5452d22919beb9e2981d48e524bd11e6
bhavana <bhavana@netlify.com>
2022-02-08 11:27:24 -0500

    changed the image optimizer to Akamai for next export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+/*module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
-};
+}; 
+*/
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}

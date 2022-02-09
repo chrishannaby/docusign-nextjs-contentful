6ff8236090112ad50faae251612133bfd1e70e31
Chris Hannaby <chris.hannaby@netlify.com>
2022-02-09 09:28:12 -0700

    run in post build

--- plugins/add-rbac/index.js
+++ plugins/add-rbac/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  async onPreBuild({ constants }) {
+  async onPostBuild({ constants }) {
     if (!process.env.ADD_RBAC) {
       ("Not adding role based access control");
       return;

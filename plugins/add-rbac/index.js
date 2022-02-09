3885bbadfb54846134b9e6697028be2a32d8c4f3
Chris Hannaby <chris.hannaby@netlify.com>
2022-02-09 08:58:03 -0700

    update plugin

--- plugins/add-rbac/index.js
+++ plugins/add-rbac/index.js
@@ -1,5 +1,23 @@
 module.exports = {
-  async onPreBuild() {
-    console.log("add rbac");
+  async onPreBuild({ netlifyConfig }) {
+    if (!process.env.ADD_RBAC) {
+      ("Not adding role based access control");
+      return;
+    }
+
+    console.log("Adding role based access control");
+    netlifyConfig.redirects.push({
+      from: "/*",
+      status: 200,
+      force: true,
+      role: ["admin"],
+    });
+
+    netlifyConfig.redirects.push({
+      from: "/*",
+      status: 401,
+      force: true,
+      to: "https://www.docusign.com",
+    });
   },
 };

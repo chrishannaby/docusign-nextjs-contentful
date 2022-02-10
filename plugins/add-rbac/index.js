f4578cda51fbfdf22ef5bf2b003a643c0670b9e3
Chris Hannaby <chris.hannaby@netlify.com>
2022-02-10 13:23:23 -0700

    put it in public

--- plugins/add-rbac/index.js
+++ plugins/add-rbac/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  async onPostBuild({ constants }) {
+  async onPreBuild({ constants }) {
     if (!process.env.ADD_RBAC) {
       ("Not adding role based access control");
       return;
@@ -14,7 +14,7 @@ module.exports = {
     /blog/* 200! Role=admin
     /blog/* / 401!
     `.trimStart();
-    const redirectsPath = path.resolve(constants.PUBLISH_DIR, "_redirects");
+    const redirectsPath = path.resolve("./public", "_redirects");
     console.log(`Writing redirects to ${redirectsPath}`);
     fs.writeFileSync(redirectsPath, redirects);
   },
